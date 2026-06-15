8827cc544e1935b650f799fe2e6cafad8c7306ee
BONO <raihanbuono@gmail.com>
2026-06-15 22:06:56 +0700

    fixed some bug

--- core/fuzzy.py
+++ core/fuzzy.py
@@ -11,23 +11,10 @@ import matplotlib.pyplot as plt
 #[R6] IF htm_weekday is Murah AND htm_weekend is Mahal AND jarak_pusat_km is Dekat AND vote_average is Rendah AND vote_count is Banyak AND jumlah_hotel_terdekat is Banyak THEN Rekomendasi is Rendah
 
 def hitung_fuzzy_tsukamoto(df, custom_params=None):
-    """
-    Fungsi untuk menghitung skor rekomendasi wisata menggunakan metode Fuzzy Tsukamoto.
-    Menerima DataFrame pariwisata dan mengembalikan DataFrame dengan tambahan kolom skor.
-    """
-    # Salin dataframe agar tidak merubah data asli
     df_fuzzy = df.copy()
 
-    # 1. Definisikan Semesta Pembicaraan (Universe) Minimal
-    x_htm = np.array([0, 500000])
-    x_jarak = np.array([0, 60])
-    x_vote_avg = np.array([1.0, 5.0])
-    x_vote_cnt = np.array([0, 82000])
-    x_hotel = np.array([0, 524])
-
-    # 2. Definisikan Parameter Trapesium [a, b, c, d]
+    # [1. Pilihan Batas Parameter]
     if custom_params is None:
-        # Jika tidak ada input dari slider Streamlit, gunakan nilai default ini
         p_htm_murah = [0, 0, 10000, 50000]
         p_htm_mahal = [10000, 50000, 500000, 500000]
         p_jarak_dekat = [0, 0, 5, 25]
@@ -39,7 +26,6 @@ def hitung_fuzzy_tsukamoto(df, custom_params=None):
         p_hotel_sedikit = [0, 0, 5, 50]
         p_hotel_banyak = [5, 50, 524, 524]
     else:
-        # Jika dari slider dipasok nilai baru, gunakan nilai kustom tersebut
         p_htm_murah = custom_params['htm_murah']
         p_htm_mahal = custom_params['htm_mahal']
         p_jarak_dekat = custom_params['jarak_dekat']
@@ -51,76 +37,53 @@ def hitung_fuzzy_tsukamoto(df, custom_params=None):
         p_hotel_sedikit = custom_params['hotel_sedikit']
         p_hotel_banyak = custom_params['hotel_banyak']
 
-    # 3. Proses Fuzzifikasi
-    df_fuzzy['mu_htm_wd_murah'] = fuzz.interp_membership(x_htm, fuzz.trapmf(x_htm, p_htm_murah), df_fuzzy['htm_weekday'])
-    df_fuzzy['mu_htm_wd_mahal'] = fuzz.interp_membership(x_htm, fuzz.trapmf(x_htm, p_htm_mahal), df_fuzzy['htm_weekday'])
-    df_fuzzy['mu_htm_we_murah'] = fuzz.interp_membership(x_htm, fuzz.trapmf(x_htm, p_htm_murah), df_fuzzy['htm_weekend'])
-    df_fuzzy['mu_htm_we_mahal'] = fuzz.interp_membership(x_htm, fuzz.trapmf(x_htm, p_htm_mahal), df_fuzzy['htm_weekend'])
-    df_fuzzy['mu_jarak_dekat'] = fuzz.interp_membership(x_jarak, fuzz.trapmf(x_jarak, p_jarak_dekat), df_fuzzy['jarak_pusat_km'])
-    df_fuzzy['mu_jarak_jauh'] = fuzz.interp_membership(x_jarak, fuzz.trapmf(x_jarak, p_jarak_jauh), df_fuzzy['jarak_pusat_km'])
-    df_fuzzy['mu_vote_avg_rendah'] = fuzz.interp_membership(x_vote_avg, fuzz.trapmf(x_vote_avg, p_vote_avg_rendah), df_fuzzy['vote_average'])
-    df_fuzzy['mu_vote_avg_tinggi'] = fuzz.interp_membership(x_vote_avg, fuzz.trapmf(x_vote_avg, p_vote_avg_tinggi), df_fuzzy['vote_average'])
-    df_fuzzy['mu_vote_cnt_sedikit'] = fuzz.interp_membership(x_vote_cnt, fuzz.trapmf(x_vote_cnt, p_vote_cnt_sedikit), df_fuzzy['vote_count'])
-    df_fuzzy['mu_vote_cnt_banyak'] = fuzz.interp_membership(x_vote_cnt, fuzz.trapmf(x_vote_cnt, p_vote_cnt_banyak), df_fuzzy['vote_count'])
-    df_fuzzy['mu_hotel_sedikit'] = fuzz.interp_membership(x_hotel, fuzz.trapmf(x_hotel, p_hotel_sedikit), df_fuzzy['jumlah_hotel_terdekat'])
-    df_fuzzy['mu_hotel_banyak'] = fuzz.interp_membership(x_hotel, fuzz.trapmf(x_hotel, p_hotel_banyak), df_fuzzy['jumlah_hotel_terdekat'])
+    # [3. Proses Fuzzifikasi] - LANGSUNG PASS DATA KE TRAPMF
+    df_fuzzy['mu_htm_wd_murah'] = fuzz.trapmf(df_fuzzy['htm_weekday'].values, p_htm_murah)
+    df_fuzzy['mu_htm_wd_mahal'] = fuzz.trapmf(df_fuzzy['htm_weekday'].values, p_htm_mahal)
+    df_fuzzy['mu_htm_we_murah'] = fuzz.trapmf(df_fuzzy['htm_weekend'].values, p_htm_murah)
+    df_fuzzy['mu_htm_we_mahal'] = fuzz.trapmf(df_fuzzy['htm_weekend'].values, p_htm_mahal)
+    df_fuzzy['mu_jarak_dekat'] = fuzz.trapmf(df_fuzzy['jarak_pusat_km'].values, p_jarak_dekat)
+    df_fuzzy['mu_jarak_jauh'] = fuzz.trapmf(df_fuzzy['jarak_pusat_km'].values, p_jarak_jauh)
+    df_fuzzy['mu_vote_avg_rendah'] = fuzz.trapmf(df_fuzzy['vote_average'].values, p_vote_avg_rendah)
+    df_fuzzy['mu_vote_avg_tinggi'] = fuzz.trapmf(df_fuzzy['vote_average'].values, p_vote_avg_tinggi)
+    df_fuzzy['mu_vote_cnt_sedikit'] = fuzz.trapmf(df_fuzzy['vote_count'].values, p_vote_cnt_sedikit)
+    df_fuzzy['mu_vote_cnt_banyak'] = fuzz.trapmf(df_fuzzy['vote_count'].values, p_vote_cnt_banyak)
+    df_fuzzy['mu_hotel_sedikit'] = fuzz.trapmf(df_fuzzy['jumlah_hotel_terdekat'].values, p_hotel_sedikit)
+    df_fuzzy['mu_hotel_banyak'] = fuzz.trapmf(df_fuzzy['jumlah_hotel_terdekat'].values, p_hotel_banyak)
 
-    # 4. Evaluasi Rule & Inferensi Tsukamoto
-    # [R1] Rekomendasi is Tinggi
+    # [4. Evaluasi Rule & Inferensi Tsukamoto]
+    # R1 sampai R6 tetap sama seperti kode asli lu...
     df_fuzzy['alpha_R1'] = np.minimum(df_fuzzy['mu_htm_wd_murah'], np.minimum(df_fuzzy['mu_htm_we_murah'], np.minimum(df_fuzzy['mu_jarak_dekat'], np.minimum(df_fuzzy['mu_vote_avg_tinggi'], np.minimum(df_fuzzy['mu_vote_cnt_banyak'], df_fuzzy['mu_hotel_banyak'])))))
     df_fuzzy['z_R1'] = df_fuzzy['alpha_R1'] * 100
 
-    # [R2] Rekomendasi is Tinggi
     df_fuzzy['alpha_R2'] = np.minimum(df_fuzzy['mu_htm_wd_murah'], np.minimum(df_fuzzy['mu_htm_we_murah'], np.minimum(df_fuzzy['mu_jarak_jauh'], np.minimum(df_fuzzy['mu_vote_avg_tinggi'], np.minimum(df_fuzzy['mu_vote_cnt_sedikit'], df_fuzzy['mu_hotel_sedikit'])))))
     df_fuzzy['z_R2'] = df_fuzzy['alpha_R2'] * 100
 
-    # [R3] Rekomendasi is Tinggi
     df_fuzzy['alpha_R3'] = np.minimum(df_fuzzy['mu_htm_wd_mahal'], np.minimum(df_fuzzy['mu_htm_we_mahal'], np.minimum(df_fuzzy['mu_jarak_dekat'], np.minimum(df_fuzzy['mu_vote_avg_tinggi'], np.minimum(df_fuzzy['mu_vote_cnt_banyak'], df_fuzzy['mu_hotel_banyak'])))))
     df_fuzzy['z_R3'] = df_fuzzy['alpha_R3'] * 100
 
-    # [R4] Rekomendasi is Rendah
     df_fuzzy['alpha_R4'] = np.minimum(df_fuzzy['mu_htm_wd_mahal'], np.minimum(df_fuzzy['mu_htm_we_mahal'], np.minimum(df_fuzzy['mu_jarak_dekat'], np.minimum(df_fuzzy['mu_vote_avg_rendah'], np.minimum(df_fuzzy['mu_vote_cnt_banyak'], df_fuzzy['mu_hotel_banyak'])))))
     df_fuzzy['z_R4'] = 100 - (df_fuzzy['alpha_R4'] * 100)
 
-    # [R5] Rekomendasi is Rendah
     df_fuzzy['alpha_R5'] = np.minimum(df_fuzzy['mu_htm_wd_mahal'], np.minimum(df_fuzzy['mu_htm_we_mahal'], np.minimum(df_fuzzy['mu_jarak_jauh'], np.minimum(df_fuzzy['mu_vote_avg_rendah'], np.minimum(df_fuzzy['mu_vote_cnt_sedikit'], df_fuzzy['mu_hotel_sedikit'])))))
     df_fuzzy['z_R5'] = 100 - (df_fuzzy['alpha_R5'] * 100)
 
-    # [R6] Rekomendasi is Rendah
     df_fuzzy['alpha_R6'] = np.minimum(df_fuzzy['mu_htm_wd_murah'], np.minimum(df_fuzzy['mu_htm_we_mahal'], np.minimum(df_fuzzy['mu_jarak_dekat'], np.minimum(df_fuzzy['mu_vote_avg_rendah'], np.minimum(df_fuzzy['mu_vote_cnt_banyak'], df_fuzzy['mu_hotel_banyak'])))))
     df_fuzzy['z_R6'] = 100 - (df_fuzzy['alpha_R6'] * 100)
 
-    # 5. Defuzzifikasi (Weighted Average)
-    total_alpha_z = (
-        (df_fuzzy['alpha_R1'] * df_fuzzy['z_R1']) +
-        (df_fuzzy['alpha_R2'] * df_fuzzy['z_R2']) +
-        (df_fuzzy['alpha_R3'] * df_fuzzy['z_R3']) +
-        (df_fuzzy['alpha_R4'] * df_fuzzy['z_R4']) +
-        (df_fuzzy['alpha_R5'] * df_fuzzy['z_R5']) +
-        (df_fuzzy['alpha_R6'] * df_fuzzy['z_R6'])
-    )
-
-    total_alpha = (
-        df_fuzzy['alpha_R1'] + df_fuzzy['alpha_R2'] + df_fuzzy['alpha_R3'] + 
-        df_fuzzy['alpha_R4'] + df_fuzzy['alpha_R5'] + df_fuzzy['alpha_R6']
-    )
-
-    # Menghitung skor final rekomendasi
+    # [5. Defuzzifikasi]
+    total_alpha_z = ((df_fuzzy['alpha_R1'] * df_fuzzy['z_R1']) + (df_fuzzy['alpha_R2'] * df_fuzzy['z_R2']) + (df_fuzzy['alpha_R3'] * df_fuzzy['z_R3']) + (df_fuzzy['alpha_R4'] * df_fuzzy['z_R4']) + (df_fuzzy['alpha_R5'] * df_fuzzy['z_R5']) + (df_fuzzy['alpha_R6'] * df_fuzzy['z_R6']))
+    total_alpha = (df_fuzzy['alpha_R1'] + df_fuzzy['alpha_R2'] + df_fuzzy['alpha_R3'] + df_fuzzy['alpha_R4'] + df_fuzzy['alpha_R5'] + df_fuzzy['alpha_R6'])
     df_fuzzy['skor_rekomendasi'] = np.where(total_alpha > 0, total_alpha_z / total_alpha, 0)
 
     return df_fuzzy
 
 def plot_kurva(judul, max_val, param_rendah, param_tinggi, label_rendah="Rendah/Murah/Dekat", label_tinggi="Tinggi/Mahal/Jauh"):
-    """
-    Fungsi untuk membuat objek plot grafik fungsi keanggotaan trapesium.
-    """
-    # Buat linspace yang rapi untuk rendering visual grafik kurva
     x_plot = np.linspace(0, max_val, 1000)
     
-    # Ambil nilai fuzzy array untuk grafik
-    x_universe = np.array([0, max_val])
-    y_rendah = fuzz.interp_membership(x_universe, fuzz.trapmf(x_universe, param_rendah), x_plot)
-    y_tinggi = fuzz.interp_membership(x_universe, fuzz.trapmf(x_universe, param_tinggi), x_plot)
+    # FIX: Langsung evaluasi x_plot ke trapmf tanpa interp_membership
+    y_rendah = fuzz.trapmf(x_plot, param_rendah)
+    y_tinggi = fuzz.trapmf(x_plot, param_tinggi)
     
     fig, ax = plt.subplots(figsize=(6, 2.5))
     ax.plot(x_plot, y_rendah, label=label_rendah, color='blue', linewidth=2)
@@ -133,6 +96,8 @@ def plot_kurva(judul, max_val, param_rendah, param_tinggi, label_rendah="Rendah/
     
     plt.tight_layout()
     return fig
+
+
 
 def dapatkan_detail_kalkulasi(df_hasil, nama_wisata):
     """
